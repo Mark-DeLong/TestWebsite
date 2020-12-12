090c58044f1bfda54ab32c0faebbd7de6e7031a0
Mark <mark@Marks-MacBook-Air.local>
2020-12-12 13:04:07 -0500

    added basic forum functionality

---
+++ JS/cssGenerator.js
@@ -0,0 +1,117 @@
+// import React from 'react'
+// import styled from 'styled-componets'
+
+//styles
+
+// * {
+//     /* max-width: 100%;
+//     font-size: 62.5%;
+// 	font-family: 'Noto Serif', serif;
+// 	color: white;
+// 	text-decoration: none;
+// 	/*border: 1px solid black;*/
+// }
+
+//universal selector
+// const universalSelector = document.querySelector("*")
+
+// universalSelector.style.width = "100%"
+// universalSelector.style.fontSize = "62.5%"
+// universalSelector.style.fontFamily = "'Noto Serif', serif"
+// universalSelector.style.color = "white"
+// universalSelector.style.textDecoration = "none"
+// universalSelector.elm.style.border = "1px solid black"
+
+// body {
+// 	/* display: flex;
+// 	flex-direction: column;
+// 	padding-left: 10%;
+// 	padding-right: 10%;
+// 	font-size: 1.6rem;
+// 	background-color: darkred; */
+// 	/*background-image: url("/assets/1.jpg");
+//   	background-repeat: no-repeat;
+//   	background-attachment: fixed;*/
+// }
+
+// const body = document.getElementsByTagName("body")
+
+// body.style.display = "flex"
+// body.style.border = "1px solid black"
+
+// /*start navigation animation*/
+// .navigation {
+//     /* display: flex; */
+// }
+
+// a {
+//     /* color: black;
+// 	border: 1px solid white;
+// 	background-color: white;
+//     transition: transform 0.1s;
+//     margin: 2px;
+//     border-radius: 10px;
+// 	transition: transform 0.2s;
+// 	box-shadow: 2.5px 2.5px white; */
+// }
+
+// a:hover {
+//     /* transition: transform 0.85s;
+// 	transform: scale(1.08);
+// 	color: white;
+// 	background-color: black;
+// 	border: 1px solid white;
+// 	box-shadow: 2.5px 2.5px ; */
+// }
+// /*end navigation and button animation*/
+
+// header, section, footer {
+// 	/* margin-top: 5%;
+// 	margin-bottom: 5%;
+// 	background-color: black;
+// 	box-shadow: 2.5px 2.5px 2.5px 2.5px ;
+// 	padding: 5%;
+// } */
+
+// h1, h2 {
+// 	/* text-shadow: 2px 2px grey;
+// 	-webkit-text-stroke: 1px white; */
+// }
+
+// h1 {
+// 	/* font-size: 8.5vw; */
+// }
+
+// h2 {
+// 	/* font-size: 2.1rem;
+// 	margin-bottom: 5%; */
+// }
+
+
+// .name {
+// 	/* display: flex;
+// 	justify-content: space-between;
+// 	margin-top: 5%;
+// 	align-items: flex-start; */
+// }
+
+// .name h1 {
+// 	/* width: 66%; */
+// }
+
+// .name img {
+// 	/* width: 33%;  */
+// }
+
+
+// /*posts*/
+
+// .post1 img, .post2 img {
+// 	/* width: 50%; */
+// }
+
+// .posts div {
+// 	/* margin-bottom: 5%; */
+// }
+
+
--- JS/htmlGenerator.js
+++ JS/htmlGenerator.js
@@ -1,65 +1,67 @@
-//header generator
-const navigation = document.createElement("nav")
-navigation.classList.add("navigation")
 
-    const button1 = document.createElement("a")
-    button1.classList.add("button1")
-    button1.textContent = "Home"
-    button1.href = "/"
+    //header generator
+    const navigation = document.createElement("nav")
+    navigation.classList.add("navigation")
 
-    const button2 = document.createElement("a")
-    button2.classList.add("button2")
-    button2.textContent = "About"
-    button2.href = "/pages/about.html"
+        const button1 = document.createElement("a")
+        button1.classList.add("button1")
+        button1.textContent = "Home"
+        button1.href = "/"
 
-    const button3 = document.createElement("a")
-    button3.classList.add("button3")
-    button3.textContent = "Contact"
-    button3.href = "/pages/contact.html"
+        const button2 = document.createElement("a")
+        button2.classList.add("button2")
+        button2.textContent = "About"
+        button2.href = "/pages/about.html"
 
-const name = document.createElement("div")
-name.classList.add("name")
+        const button3 = document.createElement("a")
+        button3.classList.add("button3")
+        button3.textContent = "Contact"
+        button3.href = "/pages/contact.html"
 
-    const title = document.createElement("h1")
-    title.textContent = "Learning to Code"
+    const titleContainer = document.createElement("div")
+    titleContainer.classList.add("titleContainer")
 
-    const titleImg = document.createElement("img")
-    titleImg.classList.add("logo")
-    titleImg.src = "/assets/logo.png"
-    
-//footer generator
-const footNavigation = document.createElement("nav")
-footNavigation.classList.add("navigation")
+        const title = document.createElement("h1")
+        title.textContent = "Learning to Code"
 
-    const footButton1 = document.createElement("a")
-    footButton1.classList.add("button1")
-    footButton1.textContent = "Home"
-    footButton1.href = "/"
+        const titleImg = document.createElement("img")
+        titleImg.classList.add("logo")
+        titleImg.src = "/assets/logo.png"
+        
+    //footer generator
+    const footNavigation = document.createElement("nav")
+    footNavigation.classList.add("navigation")
 
-    const footButton2 = document.createElement("a")
-    footButton2.classList.add("button2")
-    footButton2.onclick = " location.href='/about.html' "
-    footButton2.textContent = "About"
-    footButton2.href = "/pages/about.html"
+        const footButton1 = document.createElement("a")
+        footButton1.classList.add("button1")
+        footButton1.textContent = "Home"
+        footButton1.href = "/"
 
-    const footButton3 = document.createElement("a")
-    footButton3.classList.add("button3")
-    footButton3.textContent = "Contact"
-    footButton3.href = "/pages/contact.html"
+        const footButton2 = document.createElement("a")
+        footButton2.classList.add("button2")
+        footButton2.onclick = " location.href='/about.html' "
+        footButton2.textContent = "About"
+        footButton2.href = "/pages/about.html"
 
-// file appending structure
-const header = document.querySelector(".header")
-const footer = document.querySelector(".footer")
+        const footButton3 = document.createElement("a")
+        footButton3.classList.add("button3")
+        footButton3.textContent = "Contact"
+        footButton3.href = "/pages/contact.html"
 
-header.appendChild(navigation)
-    navigation.appendChild(button1)
-    navigation.appendChild(button2)
-    navigation.appendChild(button3)
-header.appendChild(name)
-    name.appendChild(title)
-    name.appendChild(titleImg)
-footer.appendChild(footNavigation)
-    footNavigation.appendChild(footButton1)
-    footNavigation.appendChild(footButton2)
-    footNavigation.appendChild(footButton3)
+    // file appending structure
+    const header = document.querySelector(".header")
+    const footer = document.querySelector(".footer")
 
+    header.appendChild(navigation)
+        navigation.appendChild(button1)
+        navigation.appendChild(button2)
+        navigation.appendChild(button3)
+    header.appendChild(titleContainer)
+        titleContainer.appendChild(title)
+        titleContainer.appendChild(titleImg)
+    footer.appendChild(footNavigation)
+        footNavigation.appendChild(footButton1)
+        footNavigation.appendChild(footButton2)
+        footNavigation.appendChild(footButton3)
+
+  
